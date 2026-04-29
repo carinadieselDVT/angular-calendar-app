5355fb4ebf1ae964043f2daf6b785ba7687b227f
Carina Diesel <cdiesel@dvtsoftware.com>
2026-04-29 14:38:22 +0200

    feat: Added prev and next buttons

--- src/app/calendar/pages/year/year-page.component.ts
+++ src/app/calendar/pages/year/year-page.component.ts
@@ -4,11 +4,12 @@ import { CalendarDay, CalendarMonth } from '../../../core/models/models';
 import { CalendarService } from '../../../core/services/calendar.service';
 import { YearViewComponent } from '../../components/year-view/year-view.component';
 import { HolidayStore } from '../../store/holiday.store';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'year-page',
   standalone: true,
-  imports: [CommonModule, YearViewComponent],
+  imports: [CommonModule, YearViewComponent, ButtonModule],
   templateUrl: './year-page.component.html',
 })
 export class YearPageComponent implements OnInit {
@@ -26,6 +27,21 @@ export class YearPageComponent implements OnInit {
 
   onMonthClick(month: CalendarMonth) {
     console.log('Month clicked:', month);
+  }
+
+  private changeYear(iterator: number) {
+    const year = this.currentYear() + iterator;
+
+    this.currentYear.set(year);
+    this.holidayStore.setYear(year);
+  }
+
+  prevYear() {
+    this.changeYear(-1);
+  }
+
+  nextYear() {
+    this.changeYear(1);
   }
 
   ngOnInit() {
